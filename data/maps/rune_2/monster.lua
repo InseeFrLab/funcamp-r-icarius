60eb972ce4a951c7a3616ae9c651509106a4e2d7
Arnaud Degorre <arnaud.degorre@insee.fr>
2020-07-10 19:08:15 +0200

    Fix issue #2 about save game after Tartaros boss.

--- data/maps/rune_2/monster.lua
+++ data/maps/rune_2/monster.lua
@@ -8,6 +8,21 @@ function map:on_started()
 dungeon_index = {}
 dungeon_index = 2
 
+  if game:get_value("dungeon_2_finished") == true then 
+        start_boss_sensor:set_enabled(false)
+        sensor_leaving_dungeon:set_enabled(false)
+        map:open_doors("door_rune")
+        ropa_1:set_enabled(false)
+        ropa_2:set_enabled(false)
+        ropa_3:set_enabled(false)
+        ropa_4:set_enabled(false)
+  else
+        ropa_1:immobilize()
+        ropa_2:immobilize()
+        ropa_3:immobilize()
+        ropa_4:immobilize()
+  end
+
 end
 
 -- Event called after the opening transition effect of the map,
@@ -16,10 +31,7 @@ function map:on_opening_transition_finished()
 
   sol.audio.play_sound("bounce")
   sol.audio.play_music("dark_mountain")
-  ropa_1:immobilize()
-  ropa_2:immobilize()
-  ropa_3:immobilize()
-  ropa_4:immobilize()
+
 end
 
 
