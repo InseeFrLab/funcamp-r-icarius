b2658e1b617422588794fd716f56816d19206171
Arnaud Degorre <arnaud.degorre@insee.fr>
2020-12-12 19:32:03 +0100

    Before Statis Stone Fall Fixed

--- data/maps/outside/outF1.lua
+++ data/maps/outside/outF1.lua
@@ -3,7 +3,7 @@ local game = map:get_game()
 
 
 function sensor_landslide:on_activated()
-  sol.timer.start(map, 1000, function()
+  sol.timer.start(map, 500, function()
     sol.audio.play_sound("explosion")
     outside_stone_to_grissgrass:set_enabled(true)
     game:start_dialog("outside_icarius_outF1.landslide")
