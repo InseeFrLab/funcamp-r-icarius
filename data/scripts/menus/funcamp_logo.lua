960abf747ebd8d059ed6538673c5a0c3b3bfd2f2
Arnaud Degorre <arnaud.degorre@insee.fr>
2020-12-13 14:52:18 +0100

    fix till Statis Sandia Village

--- data/scripts/menus/funcamp_logo.lua
+++ data/scripts/menus/funcamp_logo.lua
@@ -17,7 +17,7 @@ function solarus_logo_menu:on_started()
     font_size = 10,
     color = {240, 200, 56},
     horizontal_alignment = "center",
-    text = "Funcamp R - 15 décembre 2020 (v 0.9.8.1)",
+    text = "Funcamp R - 15 décembre 2020 (v 0.9.8.2)",
   })
 
   -- Start animation.
